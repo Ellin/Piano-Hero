c7cf7a7b596549648103edc00ee0e4e84547b740
Ellin Park <park.ellin@gmail.com>
2016-04-15 23:33:52 -0400

    Corrected dict nestings

--- Piano-hero-code-2.py
+++ Piano-hero-code-2.py
@@ -29,13 +29,12 @@ def get_patient_presses (press1, press2, press3, press4):
 		press_list.append(int(press4))
 	return press_list
 
+
 def read_csv(filepath):
 	with open(filepath, 'rU') as csvfile:
 		reader = csv.reader(csvfile)
 		next(reader)	#skips header row
-		my_dict = ({})
-		session_dict = ({})
-		trial_dict = ({})
+		data_dict = ({})
 
 		for row in reader:
 			patient = 'patient ' + row[0]
@@ -45,21 +44,26 @@ def read_csv(filepath):
 			press_list = get_patient_presses (row[8], row[9], row[10], row[11])
 			condition = row[18]
 
-			if patient in my_dict:
-				session_dict = my_dict[patient]
+			if patient in data_dict:
+				session_dict = data_dict[patient]
 			else:
 				session_dict = ({})
 
 			if session in session_dict:
-				trial_dict = session_dict[trial]
+				trial_dict = session_dict[session]
 			else:
 				trial_dict = ({})
 
 			trial_dict[trial] = analysis.analyze_trial(given_list, press_list)
 			trial_dict[trial]['condition'] = condition
+
+			session_dict[session] = trial_dict
+			data_dict[patient] = session_dict
 			#within trial:  correct (0,1), # hit, # substitions, # additions, order error (1,0), # balls given (1,2,3,4)
 			#analysis function returns trial dict of... {correct: x, num_hit: x, num_substitutions: x, num_additions: x, order_error: x}
 
-			print (trial_dict[trial])
+			#print (trial_dict[trial])
+	print (data_dict)
+
 
 read_csv(filepath)
